eaf0a0fbe5c7bec7c6046b7ae32f712ad3c6f513
Mounir Elmarrakchi <Mounir.Armand@gmail.com>
2020-12-13 17:36:38 -0600

    Lesson 5 - Value Types

---
+++ Lesson3Exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson3Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+namespace Lesson3Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first sentence");
+            Console.WriteLine("I am new to C#");
+            Console.WriteLine("This C# course is cool");
+            Console.WriteLine("I am learning stuff every day");
+        }
+    }
+}
+
+
+    class Dog
+    { 
+        // Attributes of the class
+        int _bodySize'
+        string _bodyColor;
+        string _energyType
+      
+        /*
+         This method will print out about how the dog does.
+            */
+        void BarkSoftLoud()
+        { 
+            Console.Writeline("The Bark is Loud")
+        }
+
+
+    }
